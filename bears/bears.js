f193dc7f57f6808b2bd95e2b0f467c022358a2b8
Dennis Peters <denjpeters@gmail.com>
2018-01-26 13:39:34 -0500

    Initial Commit

---
+++ bears/bears.js
@@ -0,0 +1,7 @@
+function changeBackground() {
+	if (document.body.style.backgroundColor == "darkred") {
+		document.body.style.backgroundColor = "black";
+	} else {
+		document.body.style.backgroundColor = "darkred";
+	}
+}
